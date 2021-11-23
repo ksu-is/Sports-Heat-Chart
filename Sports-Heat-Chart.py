9c5311e94936ea9a5ce7efa139011cb487301c77
AndrewDayton <abc.dayton@gmail.com>
2021-11-23 18:04:43 -0500

    Update Sports-Heat-Chart.py

--- Sports-Heat-Chart.py
+++ Sports-Heat-Chart.py
@@ -7,31 +7,31 @@ import numpy as np
 import pandas as pd
 import pybaseball as bb
 from pybaseball import statcast
+from pybaseball import batting_leaders
 
-screen = tk.Tk()
-screen.geometry("300x150")
-screen.title("Sports Heat Chart")
+root = tk.Tk()
+root.geometry("300x150")
+root.title("Sports Heat Chart")
 
 # data for date range
 def data():
     data = statcast(start_dt= date, end_dt= date2)
 
 # allows user to select start date
-cal = DateEntry(screen,selectmode='day')
+cal = DateEntry(root,selectmode='day')
 cal.grid(row=1,column=1,padx=15)
 def date():
     start_dt=cal.get_date()
 
 # allows user to selct end date    
-cal2 = DateEntry(screen,selectmode='day')
+cal2 = DateEntry(root,selectmode='day')
 cal2.grid(row=1,column=2,padx=15)
 def date2():
     end_dt=cal2.get_date()
 
 # Go button takes the date range and plugs it into statcast search
-b1 = Button(screen, text='Go')
+b1 = Button(root, text='Go', command=data)
+b1.grid(row=2, column=2, padx=10)
 
 
-
-
-screen.mainloop()
+root.mainloop()
